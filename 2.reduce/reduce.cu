3183809a7fffc598412ed8ce3dbc92905d11026d
Rountaboutt <a109763648@qq.com>
2025-09-17 21:59:30 +0800

    添加了reduce v1,v2,v3优化

--- 2.reduce/reduce.cu
+++ 2.reduce/reduce.cu
@@ -8,12 +8,17 @@
 const int BLOCK_SIZE = 1024;
 const int N = 1024 * 1024;
 
+#define FULL_MASK 0xFFFFFFFF
+
+// -------------------------------------------------------------------------------------
 __global__ void reduce_v0(float* input, float* output){
     __shared__ float shared[BLOCK_SIZE];
 
     unsigned int tid = threadIdx.x;
     unsigned int bid = blockIdx.x;
     unsigned int id = tid + blockDim.x * bid;
+
+    
 
     if (id < N){
         shared[tid] = input[id];    // 把当前线程的元素复制到共享内存        
@@ -22,7 +27,39 @@ __global__ void reduce_v0(float* input, float* output){
         shared[tid] = 0.f;
     }
 
+    __syncthreads();
+    for (unsigned int s = 1; s < blockDim.x; s *= 2){
+        __syncthreads();
+        if (tid % (2 * s) == 0){
+            shared[tid] += shared[tid + s]; 
+        }
+        
+    }
+    
+    __syncthreads();
+    // 该线程块的局部规约结果
+    if (tid == 0) output[bid] = shared[0]; 
+}
 
+// -------------------------------------------------------------------------------------
+// 现在全局显存做归约,解决线程闲置的问题
+__global__ void reduce_v1(float* input, float* output){
+    __shared__ float shared[BLOCK_SIZE];
+
+    unsigned int tid = threadIdx.x;
+    unsigned int bid = blockIdx.x;
+    unsigned int id = tid + 2 * blockDim.x * bid;
+
+    
+
+    if (id < N){
+        shared[tid] = input[id] + input[id + blockDim.x];    // 先在全局显存做一次归约       
+    }
+    else{
+        shared[tid] = 0.f;
+    }
+
+    __syncthreads();
     for (unsigned int s = 1; s < blockDim.x; s *= 2){
         __syncthreads();
         if (tid % (2 * s) == 0){
@@ -37,6 +74,67 @@ __global__ void reduce_v0(float* input, float* output){
 }
 
 
+// -------------------------------------------------------------------------------------
+// 解决线程束分化的问题
+
+__global__  void reduce_v2(const float* input, float* output){
+    __shared__ float shared[BLOCK_SIZE];
+    unsigned int bid = blockIdx.x;
+    unsigned int tid = threadIdx.x;
+    unsigned int id = tid + bid * blockDim.x * 2;
+    
+    shared[tid] = input[id] + input[id + blockDim.x];
+
+    __syncthreads();
+
+    for (int s = blockDim.x / 2; s >= 1; s >>= 1){
+        __syncthreads();
+
+        // 避免了线程束分化
+        // 但如果 s <= 32 ,还是会出现线程束分化
+        if (tid < s){
+            shared[tid] += shared[tid + s];
+        }
+    }
+
+    if (tid == 0) output[bid] = shared[0];
+}
+// -------------------------------------------------------------------------------------
+__device__ void warpReduce(float* cache, unsigned int tid){
+    float val = cache[tid] + cache[tid + 32];
+    for (int offset = 16; offset >= 1; offset /= 2){
+        val += __shfl_down_sync(FULL_MASK, val, offset);
+    }
+
+    cache[tid] = val;
+}
+
+__global__ void reduce_v3(const float* input, float* output){
+    __shared__ float shared[BLOCK_SIZE];
+    unsigned int bid = blockIdx.x;
+    unsigned int tid = threadIdx.x;
+    unsigned int id = tid + blockDim.x * bid * 2;
+
+    shared[tid] = input[id] + input[id + blockDim.x];
+
+    __syncthreads();
+
+    for (int s = blockDim.x / 2; s > 32; s >>= 1){
+        __syncthreads();
+        if (tid < s){
+            shared[tid] += shared[tid + s];
+        }
+    }
+
+    if (tid < 32){
+        warpReduce(shared, tid);
+    }
+    if (tid == 0){
+        output[bid] = shared[0];
+    }
+}
+
+// -------------------------------------------------------------------------------------
 float reduce_cpu(const std::vector<float> &data){
     float sum = 0.f;
     for (float val : data){
@@ -44,11 +142,11 @@ float reduce_cpu(const std::vector<float> &data){
     }
     return sum;
 }
-
+// -------------------------------------------------------------------------------------
 
 int main(){
 
-    int num_blocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    int num_blocks = ((N + BLOCK_SIZE - 1) / BLOCK_SIZE) / 2;
 
     std::vector<float> h_data(N);
 
@@ -80,19 +178,17 @@ int main(){
     cudaEventCreate(&stop);
 
     cudaEventRecord(start);
-    reduce_v0<<<num_blocks, BLOCK_SIZE>>>(d_input, d_output);
-    reduce_v0<<<1, num_blocks>>>(d_output, d_final_output);
+    reduce_v3<<<num_blocks, BLOCK_SIZE>>>(d_input, d_output);
+    reduce_v3<<<1, num_blocks>>>(d_output, d_final_output);
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     
-
-
 
     float gpu_time = 0;
     cudaEventElapsedTime(&gpu_time, start, stop);
     cudaMemcpy(&gpu_res, d_final_output, sizeof(float), cudaMemcpyDeviceToHost);  
 
-    std::cout<<"gpu res:"<<gpu_res<<std::endl;
+    std::cout<<"gpu result:"<<gpu_res<<std::endl;
     std::cout<<"gpu time:"<<gpu_time<<"ms"<<std::endl;
     
     cudaEventDestroy(start);

1e303fe2d17b291649b758a5bb644627b9f6e02a
Rountaboutt <a109763648@qq.com>
2025-10-04 16:22:42 +0800

    重写了matmul1

--- 3.matmul/matmul1.cu
+++ 3.matmul/matmul1.cu
@@ -5,27 +5,23 @@
 
 #define BLOCKSIZE 32
 
-//-------------------------------------------------------------------------------------------------------
-__global__ void mysgemm_v1(int M, int N, int K, float alpha, float* A, float* B, float beta, float* C){
+__global__ void sgemm_v1(int M, int N, int K, float* A, float* B, float* C, float alpha, float beta){
+    int id_x = threadIdx.x + blockIdx.x * blockDim.x;
+    int id_y = threadIdx.y + blockIdx.y * blockDim.y;
 
-    // gy负责A(M-K)中的一行, gx负责B(K-N)中的一列
-    // gx是列索引, gy是行索引
-    int gx = threadIdx.x + blockDim.x * blockIdx.x;
-    int gy = threadIdx.y + blockDim.y * blockIdx.y;
+    if(id_x >= N || id_y >= M) return;
 
-    if (gx >= N || gy >= M) return;
-
-    float temp = 0.0f;
+    float temp = 0.f;
     for (int i = 0; i < K; i++){
-        temp += A[gy * K + i] * B[i * N + gx];      // 两次访问全局显存, 效率低下
+        temp += A[id_y * K + i] * B[i * N + id_x];
     }
 
-    C[gy * N + gx] = alpha * temp + beta * C[gy * N + gx];
+    C[id_y * N + id_x] = alpha * temp + beta * C[id_y * N + id_x];
 }
+
 
 int main(){
     std::vector<int> sizes = {128, 256, 512, 1024, 2048, 4096, 8192};
-
     for (int N:sizes){
         size_t elemCount = N * N;
         std::cout<<"------------------------Testing size: "<< N <<"------------------------"<< std::endl;
@@ -34,12 +30,12 @@ int main(){
         float* A = (float*)malloc(numBytes);
         float* B = (float*)malloc(numBytes);
         float* C_cublas = (float*)malloc(numBytes);
-        float* C_v1 = (float*)malloc(numBytes);
+        float* C_sgemm = (float*)malloc(numBytes);
 
-        float* d_A,* d_B, * d_C_v1;
+        float* d_A,* d_B, * d_C_sgemm;
         cudaMalloc(&d_A, numBytes);
         cudaMalloc(&d_B, numBytes);
-        cudaMalloc(&d_C_v1, numBytes);
+        cudaMalloc(&d_C_sgemm, numBytes);
 
         try{
             for (int i = 0; i < elemCount; i++){
@@ -57,14 +53,14 @@ int main(){
             float beta = 0.f;
             
 
-            /*------------------------cublas计算/*------------------------*/
+            /*------------------------cublas计算------------------------*/
             cudaEvent_t start, end;
             cudaEventCreate(&start);
             cudaEventCreate(&end);
 
-            int warmup_time = 10;
+            int warmup_time = 5;
             for (int i = 0; i < warmup_time; i++){
-                cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &alpha, d_B, N, d_A, N, &beta, d_C_v1, N);
+                cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &alpha, d_B, N, d_A, N, &beta, d_C_sgemm, N);
             }
 
             cudaDeviceSynchronize();
@@ -72,7 +68,7 @@ int main(){
             int repeat_time = 5;
             cudaEventRecord(start);
             for (int i = 0; i < repeat_time; i++){
-                cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &alpha, d_B, N, d_A, N, &beta, d_C_v1, N);
+                cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &alpha, d_B, N, d_A, N, &beta, d_C_sgemm, N);
             }
             cudaEventRecord(end);
             cudaEventSynchronize(end);
@@ -80,21 +76,21 @@ int main(){
             float cublas_time = 0;
             cudaEventElapsedTime(&cublas_time, start, end);
             
-            cudaMemcpy(C_cublas, d_C_v1, numBytes, cudaMemcpyDeviceToHost);
+            cudaMemcpy(C_cublas, d_C_sgemm, numBytes, cudaMemcpyDeviceToHost);
             std::cout<<"cublas time:"<<cublas_time<<"ms"<<std::endl;
 
             /*------------------------v1计算------------------------*/
             dim3 threads(BLOCKSIZE, BLOCKSIZE);
-            dim3 blocks((N + threads.x - 1) / threads.x, (N + threads.y - 1) / threads.y);
+            dim3 blocks((N + threads.x - 1) / threads.x, (N + threads.x - 1) / threads.x);
             
             for (int i = 0; i < warmup_time; i++){
-                mysgemm_v1<<<blocks, threads>>>(N, N, N, alpha, d_A, d_B, beta, d_C_v1);
+                sgemm_v1<<<blocks, threads>>>(N, N, N, d_A, d_B, d_C_sgemm, alpha, beta);
             }
             cudaDeviceSynchronize();
 
             cudaEventRecord(start);
             for (int i = 0; i < repeat_time; i++){
-                mysgemm_v1<<<blocks, threads>>>(N, N, N, alpha, d_A, d_B, beta, d_C_v1);
+                sgemm_v1<<<blocks, threads>>>(N, N, N, d_A, d_B, d_C_sgemm, alpha, beta);
             }
             cudaEventRecord(end);
             cudaEventSynchronize(end);
@@ -102,14 +98,15 @@ int main(){
             float v1_time = 0.f;
             cudaEventElapsedTime(&v1_time, start, end);
 
-            cudaMemcpy(C_v1, d_C_v1, numBytes, cudaMemcpyDeviceToHost);
-            std::cout<<"v1 time:"<<v1_time<<"ms"<<std::endl;
+            cudaMemcpy(C_sgemm, d_C_sgemm, numBytes, cudaMemcpyDeviceToHost);
+            std::cout<<"v1 time:"<< v1_time <<"ms"<<std::endl;
 
+            std::cout<< "sgemm' GFLOPS is "<< cublas_time / v1_time * 100 << "% of cublas" << std::endl; 
             
             // 结果比较
             bool isMatch = true;
             for (int i = 0; i < elemCount; i++){
-                if (fabsf(C_cublas[i] - C_v1[i]) > 1e-3){
+                if (fabsf(C_cublas[i] - C_sgemm[i]) > 1e-5){
                     isMatch = false;
                     break;
                 }
@@ -120,7 +117,7 @@ int main(){
             
         }
         catch(...){
-            std::cerr << "Out of memory or error during testing size: " << N << std::endl;
+            std::cerr << "Error testing size: " << N << std::endl;
         }
     }
     
--- 3.matmul/matmul5.cu
+++ 3.matmul/matmul5.cu
@@ -7,6 +7,7 @@
 
 #define OFFSET(row, col ,ld) ((row) * (ld) + (col))
 #define FETCH_FLOAT4(pointer) (reinterpret_cast<float4*>(&(pointer))[0])
+
 template <const int BM, const int BN, const int BK, const int TM, const int TN>
 __global__  void __launch_bounds__(256) mysgemm_v5(int M, int N, int K, float alpha, float* A, float* B, float beta, float* C){
     const int bx = blockIdx.x;
